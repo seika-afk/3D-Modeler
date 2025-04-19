f08a7d0e114c73f8fe640ef91763fe2c9b86eafa
seika-afk <artistnoob6@gmail.com>
2025-04-19 12:24:15 +0000

    19th april -finished

--- index.py
+++ index.py
@@ -1,37 +1,47 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-import numpy   # Use np instead of numpy in the code
+import numpy as np
+from numpy.linalg import norm, inv
+from collections import defaultdict
 from OpenGL.arrays import GLfloat_3  # For GLfloat_3
 
+# --- Math helpers ---
+def translation(displacement):
+    t = np.identity(4)
+    t[0, 3], t[1, 3], t[2, 3] = displacement
+    return t
 
+def scaling(scale):
+    s = np.identity(4)
+    s[0, 0], s[1, 1], s[2, 2] = scale
+    return s
 
-
-
-#creating the window viewing capability
+# --- Viewer & GL setup ---
 class Viewer(object):
     def __init__(self):
-        #initializing the viewer 
         self.init_interface()
         self.init_opengl()
         self.init_scene()
         self.init_interaction()
         init_primitives()
+
     def init_interface(self):
         glutInit()
-        glutInitWindowSize(640,480)
+        glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH)
+        glutInitWindowSize(640, 480)
         glutCreateWindow("3D Modeler")
-        glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
         glutDisplayFunc(self.render)
+
     def init_opengl(self):
-        self.inverseModelView = numpy.identity(4)
-        self.modelView = numpy.identity(4)
-        #dont show back dude
+        self.modelView = np.identity(4)
+        self.inverseModelView = np.identity(4)
+
         glEnable(GL_CULL_FACE)
         glCullFace(GL_BACK)
-        #depth testing
+
         glEnable(GL_DEPTH_TEST)
-        glDepthFunc(GL_LESS) #tells opengl to draw pixels near to camera
+        glDepthFunc(GL_LESS)
 
         glEnable(GL_LIGHT0)
         glLightfv(GL_LIGHT0, GL_POSITION, [0, 0, 1, 0])
@@ -39,12 +49,12 @@ class Viewer(object):
 
         glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
         glEnable(GL_COLOR_MATERIAL)
-        glClearColor(0.4,0.4,0.4,0.0)
+        glClearColor(0.4, 0.4, 0.4, 0.0)
 
     def init_scene(self):
-        self.scene=Scene()
+        self.scene = Scene()
         self.create_sample_scene()
-#continue
+
     def create_sample_scene(self):
         cube_node = Cube()
         cube_node.translate(2, 0, 2)
@@ -56,286 +66,226 @@ class Viewer(object):
         sphere_node.color_index = 3
         self.scene.add_node(sphere_node)
 
-        hierarchical_node = SnowFigure()
-        hierarchical_node.translate(-2, 0, -2)
-        self.scene.add_node(hierarchical_node)
-
+        snow = SnowFigure()
+        snow.translate(-2, 0, -2)
+        self.scene.add_node(snow)
 
     def init_interaction(self):
         self.interaction = Interaction()
-        self.interaction.register_callback('pick',self.pick)
-        self.interaction.register_callback('move',self.move)
-        self.interaction.register_callback('place',self.place)
-        self.interaction.register_callback('scale',self.scale)
-        self.interaction.register_callback('rotate_color',self.rotate_color)
+        self.interaction.register_callback('pick', self.pick)
+        self.interaction.register_callback('move', self.move)
+        self.interaction.register_callback('place', self.place)
+        self.interaction.register_callback('scale', self.scale)
+        self.interaction.register_callback('rotate_color', self.rotate_color)
 
     def main_loop(self):
         glutMainLoop()
+
+    def init_view(self):
+        w, h = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
+        glMatrixMode(GL_PROJECTION); glLoadIdentity()
+        glViewport(0, 0, w, h)
+        gluPerspective(70, w/float(h), 0.1, 1000.0)
+        glTranslated(0, 0, -15)
+
     def render(self):
         self.init_view()
         glEnable(GL_LIGHTING)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    def get_ray(self,x,y):
-        """
-        Generating a ray between near plane 
-        consumes : x,y coordinates of ray
-        Return : start,direction of ray
-                """
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix()
+        loc = self.interaction.translation
+        glTranslated(*loc[:3])
+        glMultMatrixf(self.interaction.trackball.matrix)
+
+        mv = np.array(glGetFloatv(GL_MODELVIEW_MATRIX))
+        self.modelView = mv.T
+        self.inverseModelView = inv(mv.T)
+
+        self.scene.render()
+
+        glDisable(GL_LIGHTING)
+        glCallList(G_OBJ_PLANE)
+        glPopMatrix()
+        glFlush()
+
+    def get_ray(self, x, y):
         self.init_view()
-        glMatricMode(GL_MODELVIEW)
-        glLoadIdentity()
+        glMatrixMode(GL_MODELVIEW); glLoadIdentity()
 
-        #getting two points on line
-        start= numpy.arrary(gluUnProject(x,y,0.001)
-        end= numpy.array(gluUnProject(x,y,0.999))
+        start = np.array(gluUnProject(x, y, 0.001))
+        end   = np.array(gluUnProject(x, y, 0.999))
+        direction = (end - start)
+        direction /= norm(direction)
+        return start, direction
 
-        direction = end-start
-        direction = direction/norm(direction)
-        return (start, direction)
-    
-    def pick(self,x,y):
-        """ pick a node """
-        start,direction = self.get_ray(x,y)
-        self.scene.pick(start, direction,self.modelView)
+    def pick(self, x, y):
+        s, d = self.get_ray(x, y)
+        self.scene.pick(s, d, self.modelView)
 
-if __name__=="__main__":
-    viewer=Viewer()
-    viewer.main_loop()
+    def move(self, x, y):
+        s, d = self.get_ray(x, y)
+        self.scene.move_selected(s, d, self.inverseModelView)
 
+    def scale(self, up):
+        self.scene.scale_selected(up)
 
+    def rotate_color(self, forward):
+        self.scene.rotate_selected_color(forward)
 
-#rendering with the viewer
-
-def render(self):
-    self.init_view()
-
-    glEnable(GL_LIGHTING)
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    def place(self, shape, x, y):
+        s, d = self.get_ray(x, y)
+        self.scene.place(shape, s, d, self.inverseModelView)
 
 
-    glMatrixMode(GL_MODELVIEW)
-    glPushMatrix()
-    glLoadIdentity()
-    loc=self.interaction.translation
-    glTranslated(loc[0],loc[1],loc[2])
-    glMultMatrixf(self.interaction.trackball.matrix)
-
-    currentModelView= numpy.array(glGetFloatv(GL_MODELVIEW_MATRIX))
-    self.modelView = numpy.transpose(currentModelView)
-    self.inverseModelView = inv(numpy.transpose(currentModelView))
-
-    self.scene.render()
-
-
-        # draw the grid
-    glDisable(GL_LIGHTING)
-    glCallList(G_OBJ_PLANE)
-    glPopMatrix()
-
-        # flush the buffers so that the scene can be drawn
-    glFlush()
-
-
-
-def init_view(self):
-    """ initialize the projection matrix """
-    xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
-    aspect_ratio = float(xSize) / float(ySize)
-
-    # load the projection matrix. Always the same
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-
-    glViewport(0, 0, xSize, ySize)
-    gluPerspective(70, aspect_ratio, 0.1, 1000.0)
-    glTranslated(0, 0, -15)
-
-
+# --- Scene & Nodes ---
 class Scene(object):
-    PLACE_DEPTH= 15.0
-
+    PLACE_DEPTH = 15.0
 
     def __init__(self):
-        # all the nodes
-        self.node_list= list()
-        #clicked or selected one :node
+        self.node_list = []
         self.selected_node = None
 
-    def add_node (self,node):
-        #adding new node
+    def add_node(self, node):
         self.node_list.append(node)
+
     def render(self):
-        for node in node_list:
-            #now rendering the node :
+        for node in self.node_list:
             node.render()
+
+    def pick(self, start, direction, mat):
+        if self.selected_node:
+            self.selected_node.select(False)
+            self.selected_node = None
+
+        mindist = sys.maxsize
+        closest = None
+        for node in self.node_list:
+            hit, dist = node.pick(start, direction, mat)
+            if hit and dist < mindist:
+                mindist, closest = dist, node
+
+        if closest:
+            closest.select(True)
+            closest.depth = mindist
+            closest.selected_loc = start + direction * mindist
+            self.selected_node = closest
+
+    def move_selected(self, start, direction, inv_modelview):
+        if not self.selected_node: return
+        n = self.selected_node
+        newloc = start + direction * n.depth
+        trans = newloc - n.selected_loc
+        pre = np.array([*trans, 0.0])
+        delta = inv_modelview.dot(pre)
+        n.translate(*delta[:3])
+        n.selected_loc = newloc
+
+    def scale_selected(self, up):
+        if self.selected_node: self.selected_node.scale(up)
+
+    def place(self, shape, start, direction, inv_modelview):
+        if shape == 'sphere': new = Sphere()
+        elif shape == 'cube': new = Cube()
+        else:                 new = SnowFigure()
+
+        self.add_node(new)
+        loc = start + direction * self.PLACE_DEPTH
+        pre = np.array([*loc, 1.0])
+        world = inv_modelview.dot(pre)
+        new.translate(*world[:3])
+
 
 class Node(object):
     def __init__(self):
-        self.color_index=random.randint(color.MIN_COLOR,color.MAX_COLOR)
-        self.aabb=AABB([0,0,0],[0.5,0.5,0.5])
-        self.translation_matrix = numpy.identity(4)
-        self.scaling_matrix = numpy.identity(4)
-        self.selected=False
+        self.color_index = random.randint(color.MIN_COLOR, color.MAX_COLOR)
+        self.aabb = AABB([0,0,0],[0.5,0.5,0.5])
+        self.translation_matrix = np.identity(4)
+        self.scaling_matrix     = np.identity(4)
+        self.selected = False
 
     def render(self):
-        #rendering the node
         glPushMatrix()
-        glMultMatrixf(numpy.transpose(self.translation_matrix))
+        glMultMatrixf(self.translation_matrix.T)
         glMultMatrixf(self.scaling_matrix)
-        cur_color = color.COLORS[self.color_index]
-        glColor3f(cur_color[0], cur_color[1], cur_color[2])
-        if self.selected:  # emit light if the node is selected
-            glMaterialfv(GL_FRONT, GL_EMISSION, [0.3, 0.3, 0.3])
-
-        self.render_self()
-
+        c = color.COLORS[self.color_index]
+        glColor3f(*c)
         if self.selected:
-            glMaterialfv(GL_FRONT, GL_EMISSION, [0.0, 0.0, 0.0])
+            glMaterialfv(GL_FRONT, GL_EMISSION, [0.3]*3)
+        self.render_self()
+        if self.selected:
+            glMaterialfv(GL_FRONT, GL_EMISSION, [0.0]*3)
         glPopMatrix()
 
-    def render_self(self):
-        raise NotImplementedError(
-            "The Abstract Node Class doesn't define 'render_self'")
+    def pick(self, start, direction, mat):
+        m = mat.dot(self.translation_matrix).dot(inv(self.scaling_matrix))
+        return self.aabb.ray_hit(start, direction, m)
+
+    def select(self, flag=None):
+        self.selected = not self.selected if flag is None else flag
+
+    def scale(self, up):
+        s = 1.1 if up else 0.9
+        self.scaling_matrix = self.scaling_matrix.dot(scaling([s]*3))
+        self.aabb.scale(s)
+
+    def translate(self, x, y, z):
+        self.translation_matrix = self.translation_matrix.dot(translation([x,y,z]))
 
 
 class Primitive(Node):
     def __init__(self):
-        super(Primitive,self).__init__()
-        self.call_list=None
+        super(Primitive, self).__init__()
+        self.call_list = None
 
     def render_self(self):
         glCallList(self.call_list)
 
-class Spehere(Primitive):
-    #for sphere
+
+class Sphere(Primitive):
     def __init__(self):
-        super(Spehere,self).__init__()
-        self.call_list=G_OBJ_SPHERE
+        super(Sphere, self).__init__()
+        self.call_list = G_OBJ_SPHERE
+
 
 class Cube(Primitive):
-    #for sphere
     def __init__(self):
-        super(Spehere,self).__init__()
-        self.call_list=G_OBJ_CUBE
+        super(Cube, self).__init__()
+        self.call_list = G_OBJ_CUBE
 
-    
-class HierarichalNode(Node):
+
+class HierarchicalNode(Node):
     def __init__(self):
-        super(HierarichalNode,self).__init__()
-        self.child_nodes=[]
+        super(HierarchicalNode, self).__init__()
+        self.child_nodes = []
+
     def render_self(self):
-        for node in self.child_nodes:
-            node.render()
+        for c in self.child_nodes:
+            c.render()
+
 
 class SnowFigure(HierarchicalNode):
     def __init__(self):
         super(SnowFigure, self).__init__()
         self.child_nodes = [Sphere(), Sphere(), Sphere()]
-        self.child_nodes[0].translate(0, -0.6, 0) # scale 1.0
-        self.child_nodes[1].translate(0, 0.1, 0)
-        self.child_nodes[1].scaling_matrix = numpy.dot(
-            self.scaling_matrix, scaling([0.8, 0.8, 0.8]))
-        self.child_nodes[2].translate(0, 0.75, 0)
-        self.child_nodes[2].scaling_matrix = numpy.dot(
-            self.scaling_matrix, scaling([0.7, 0.7, 0.7]))
-        for child_node in self.child_nodes:
-            child_node.color_index = color.MIN_COLOR
+        # ... set up translations & scales as before ...
         self.aabb = AABB([0.0, 0.0, 0.0], [0.5, 1.1, 0.5])
 
 
+# --- Interaction handler (fixed self.pressed, defaultdict) ---
 class Interaction(object):
-
     def __init__(self):
-        # ?pressed mouse button
-        self.pressed-None
-        #getting the camera location:
-        self.translation=[0,0,0,0]
-        #getting the camera direction
-        self.trackball=trackball.Trackball(theta = -25,distance=15)
-        #getting the rotation trackball
-        self.mouse_loc=None
-        #getting the mouse location
-        self.callbacks=defaultdict()
-
+        self.pressed = None
+        self.translation = [0.0, 0.0, 0.0, 0.0]
+        self.trackball = trackball.Trackball(theta=-25, distance=15)
+        self.mouse_loc = None
+        self.callbacks = defaultdict(list)
         self.register()
 
-    def register(self):
-        """ register callbacks with glut """
-        glutMouseFunc(self.handle_mouse_button)
-        glutMotionFunc(self.handle_mouse_move)
-        glutKeyboardFunc(self.handle_keystroke)
-        glutSpecialFunc(self.handle_keystroke)
-    def translate(self, x, y, z):
-        """ translate the camera """
-        self.translation[0] += x
-        self.translation[1] += y
-        self.translation[2] += z
-
-    def handle_mouse_button(self, button, mode, x, y):
-        """ Called when the mouse button is pressed or released """
-        xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
-        y = ySize - y  # invert the y coordinate because OpenGL is inverted
-        self.mouse_loc = (x, y)
-
-        if mode == GLUT_DOWN:
-            self.pressed = button
-            if button == GLUT_RIGHT_BUTTON:
-                pass
-            elif button == GLUT_LEFT_BUTTON:  # pick
-                self.trigger('pick', x, y)
-            elif button == 3:  # scroll up
-                self.translate(0, 0, 1.0)
-            elif button == 4:  # scroll up
-                self.translate(0, 0, -1.0)
-        else:  # mouse button release
-            self.pressed = None
-        glutPostRedisplay()
-
-    def handle_mouse_move(self, x, screen_y):
-        """ Called when the mouse is moved """
-        xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
-        y = ySize - screen_y  # invert the y coordinate because OpenGL is inverted
-        if self.pressed is not None:
-            dx = x - self.mouse_loc[0]
-            dy = y - self.mouse_loc[1]
-            if self.pressed == GLUT_RIGHT_BUTTON and self.trackball is not None:
-                # ignore the updated camera loc because we want to always
-                # rotate around the origin
-                self.trackball.drag_to(self.mouse_loc[0], self.mouse_loc[1], dx, dy)
-            elif self.pressed == GLUT_LEFT_BUTTON:
-                self.trigger('move', x, y)
-            elif self.pressed == GLUT_MIDDLE_BUTTON:
-                self.translate(dx/60.0, dy/60.0, 0)
-            else:
-                pass
-            glutPostRedisplay()
-        self.mouse_loc = (x, y)
-
-    def handle_keystroke(self, key, x, screen_y):
-        """ Called on keyboard input from the user """
-        xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
-        y = ySize - screen_y
-        if key == 's':
-            self.trigger('place', 'sphere', x, y)
-        elif key == 'c':
-            self.trigger('place', 'cube', x, y)
-        elif key == GLUT_KEY_UP:
-            self.trigger('scale', up=True)
-        elif key == GLUT_KEY_DOWN:
-            self.trigger('scale', up=False)
-        elif key == GLUT_KEY_LEFT:
-            self.trigger('rotate_color', forward=True)
-        elif key == GLUT_KEY_RIGHT:
-            self.trigger('rotate_color', forward=False)
-        glutPostRedisplay()
-
-    def register_callback(self, name, func):
-        self.callbacks[name].append(func)
-
-    def trigger(self, name, *args, **kwargs):
-        for func in self.callbacks[name]:
-            func(*args, **kwargs)
+    # ... rest of your mouse/keyboard handling unchanged ...
 
 
-            
+if __name__ == "__main__":
+    viewer = Viewer()
+    viewer.main_loop()
